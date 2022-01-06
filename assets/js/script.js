da0bcc7a7084ce9a104438de38f7a94d83a7025b
nitrotap <kartikinpublic@gmail.com>
2022-01-05 21:07:28 -0700

    adding length and case functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,60 @@
 // Assignment code here
 
 
+function setLength() {
+  var inputLength = window.prompt("What length password? 8-128 characters");
+  if (inputLength >= 8 && inputLength <= 128) {
+    length = inputLength;
+  } else {
+    window.alert("Incorrect Length! You entered: " + inputLength + ". You need to enter a number between 8 and 128.");
+    setLength();
+  }
+
+}
+
+function setLowerCase() {
+  var inputLowerCase = window.prompt(
+    "Should your password include lowercase letters? Y or N"
+  );
+  switch (inputLowerCase) {
+    case "yes":
+    case "Y":
+    case "y":
+    case "YES":
+      lowerCase = true;
+      break;
+    default:
+      lowerCase = false;  
+  }
+  window.alert("lowercase use: " + lowerCase);
+}
+
+function setUpperCase() {
+  var inputUpperCase = window.prompt(
+    "Should your password include UPPERCASE letters? Y or N"
+  );
+  if (inputUpperCase === "yes" || inputUpperCase === "YES" || inputUpperCase === "Y" || inputUpperCase === "y") {
+    upperCase = true;
+  } else {
+    upperCase = false;
+  }
+  window.alert("uppercase use: " + upperCase);
+}
+
+function useInteger() {
+  var inputInteger = window.prompt(
+    "Should your password include numbers 0-9? Y or N"
+  );
+}
+
+
+// length 8-128 chars
+// charTypes lowercase, uppercase, numeric, and/or special characters
+function generatePassword(length, charTypes) {
+
+  return password;
+}
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -15,3 +69,10 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+while (true) {
+
+
+  setLength();
+  setLowerCase();
+}
