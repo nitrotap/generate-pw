7dbbd059b6b262167e7640b75f852195c4856f01
nitrotap <kartikinpublic@gmail.com>
2022-01-07 01:22:01 -0700

    adding setters for generator

---
+++ assets/js/scratch.js
@@ -0,0 +1,16 @@
+randomNum = function (min, max) {
+    var value = Math.floor((Math.random() * max) + min);
+    return value;
+}
+
+var a = randomNum(97, 122); // returns int
+console.log(a);
+var b = String.fromCharCode(b);
+console.log(b);
+
+
+
+
+
+Math.floor((Math.random() * 10) + 1);
+Math.floor((Math.random() * 10) + 1);
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
 // Assignment code here
 
+var randomNum = function (min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + 1);
+  return value;
+}
 
-function setLength() {
+function setLength() { // returns length
   var inputLength = window.prompt("What length password? 8-128 characters");
   if (inputLength >= 8 && inputLength <= 128) {
     length = inputLength;
@@ -9,48 +13,118 @@ function setLength() {
     window.alert("Incorrect Length! You entered: " + inputLength + ". You need to enter a number between 8 and 128.");
     setLength();
   }
-
+  return length;
 }
 
-function setLowerCase() {
+function setLowerCase() { // returns lowerCase
   var inputLowerCase = window.prompt(
     "Should your password include lowercase letters? Y or N"
   );
+  inputLowerCase = inputLowerCase.toLowerCase;
   switch (inputLowerCase) {
     case "yes":
-    case "Y":
     case "y":
-    case "YES":
       lowerCase = true;
       break;
     default:
-      lowerCase = false;  
+      lowerCase = false;
   }
   window.alert("lowercase use: " + lowerCase);
+  return lowerCase;
 }
 
-function setUpperCase() {
+// get a random lowercase letter
+function getLowerCase() {
+  // 26 letters in the alpabet - UTF codes 97-122
+  // get letter number and use String.fromCharCode() to get actual letter
+  letter = String.fromCharCode(randomNum(97, 122));
+
+}
+
+function setUpperCase() { // returns upperCase
   var inputUpperCase = window.prompt(
     "Should your password include UPPERCASE letters? Y or N"
   );
-  if (inputUpperCase === "yes" || inputUpperCase === "YES" || inputUpperCase === "Y" || inputUpperCase === "y") {
+  inputUpperCase = inputUpperCase.toLowerCase;
+  var upperCase = true;
+  if (inputUpperCase === "yes" || inputUpperCase === "y") {
     upperCase = true;
   } else {
     upperCase = false;
   }
   window.alert("uppercase use: " + upperCase);
+  return upperCase;
 }
 
-function useInteger() {
-  var inputInteger = window.prompt(
+function setNumeric() { // returns setNumeric
+  var inputNumeric = window.prompt(
     "Should your password include numbers 0-9? Y or N"
   );
+  inputNumeric = inputNumeric.toLowerCase();
+  setNumeric = true;
+  switch (inputNumeric) {
+    case "yes":
+    case "y":
+      setNumeric = true;
+      break;
+    default:
+      setNumeric = false;
+  }
+  window.alert("Integer use: " + setNumeric);
+  return setNumeric;
 }
 
+function setSpecialCharacter() { // returns setSpecial
+  var inputSpecialCharacters = window.prompt(
+    "Should your password include special characters? Y or N"
+  );
+  inputSpecialCharacters = inputSpecialCharacters.toLowerCase();
+  switch (inputSpecialCharacters) {
+    case "yes":
+    case "y":
+      setSpecialCharacters = true;
+      break;
+    default:
+      setSpecialCharacters = false;
+  }
+  window.alert("Special Character use: " + setSpecialCharacters);
+  return setSpecialCharacters;
+}
 
 // length 8-128 chars
 // charTypes lowercase, uppercase, numeric, and/or special characters
-function generatePassword(length, charTypes) {
+function generatePassword(length, lowerCase, upperCase, setNumeric, setSpecialCharacters) {
+
+  for (var i = 0; i < length - 1; i++) {
+    // pick at random a function from 4 available methods
+    methodNum = randomNum(1, 4);
+
+    switch (methodNum) {
+      case 1:
+        getLowerCase();
+        break;
+      case 2:
+        // get upper case
+      
+        break;
+      case 3:
+        // get numeric value
+
+        break;
+      case 4:
+        // get special character  
+
+        break;
+      default:
+        // default back up
+
+    }
+
+
+
+
+
+  }
 
   return password;
 }
@@ -71,8 +145,10 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 while (true) {
-
-
   setLength();
   setLowerCase();
+  setUpperCase();
+  setNumeric();
+  setSpecialCharacters;
+  set
 }
